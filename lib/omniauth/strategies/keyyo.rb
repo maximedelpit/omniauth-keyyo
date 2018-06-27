571e152992dabfd4ff0def88a85b49b22669eb10
Maxime Delpit <maximedelpit@gmail.com>
2018-06-27 14:29:10 +0200

    bpry to test implement°

--- lib/omniauth/strategies/keyyo.rb
+++ lib/omniauth/strategies/keyyo.rb
@@ -14,6 +14,7 @@ module OmniAuth
         token_url: 'https://api.keyyo.com/oauth2/token.php'
       }
 
+      # option :token_params, {grant_type: 'authorization_code'}
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
       # additional calls (if the user id is returned with the token
@@ -34,11 +35,27 @@ module OmniAuth
         }
       end
 
+       # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+      def callback_url
+        options[:callback_url] || (full_host + script_name + callback_path)
+      end
+
       # { "access_token":"<Your access token>","expires_in":3600,"token_type":"bearer","scope":"a.user o.w.voipprofile","refresh_token":"<Your refresh token>" }
 
       def raw_info
-        binding.pry
         @raw_info ||= access_token.get('/').parsed
+      end
+
+      def authorize_params
+        super
+      end
+
+      def build_access_token
+        super
+        # session["omniauth.state"] #= params[:state]
+        # verifier = request.params["code"]
+        # state = request.params["state"]
+        # client.auth_code.get_token(verifier, {redirect_uri: callback_url, state: state}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
       end
     end
   end
